c699e3a284f329c984cca08d7891daba7089a02c
YukiUmetsu <yuuki.umetsu@gmail.com>
2016-09-24 21:26:52 +0900

    added recipe service, shopping-list service

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,22 +1,21 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import {Recipe} from "../recipe";
+import {RecipeService} from "../recipe.service";
 
 @Component({
   selector: 'rb-recipe-list',
   templateUrl: './recipe-list.component.html'
 })
 export class RecipeListComponent implements OnInit {
-  recipes: Recipe[] = [
-    new Recipe('curry', 'indian curry', 'http://ichef.bbci.co.uk/food/ic/food_16x9_608/recipes/chickenjalfrezi_91772_16x9.jpg', []),
-    new Recipe('tomato pasta', 'tomato Pasta dish', 'http://www.wikihow.com/images/3/38/Make-a-Delicious-Dish-of-Spaghetti-and-Tomato-Sauce-Step-18.jpg', []),
-    new Recipe('pizza', 'pizza description', 'http://netgeek.biz/wp-content/uploads/2015/12/pizza_newyork5.jpg', [])
-  ];
+  recipes: Recipe[] = [];
+
   @Output() recipeSelected = new EventEmitter<Recipe>();
   //recipe = new Recipe('dummy', 'Dummy description', 'http://lehmanlaw.mn/wp-content/uploads/2016/03/vga-1.png');
 
-  constructor() { }
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
+    this.recipes = this.recipeService.getRecipes();
   }
 
   onSelected(recipe: Recipe){
---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import {Recipe} from "./recipe";
+import {Ingredient} from "../shared/ingredient";
+
+@Injectable()
+export class RecipeService {
+
+  private recipes: Recipe[] = [
+    new Recipe('curry', 'indian curry', 'http://ichef.bbci.co.uk/food/ic/food_16x9_608/recipes/chickenjalfrezi_91772_16x9.jpg', [
+        new Ingredient('curry powder', 2),
+        new Ingredient('chicken', 1)
+    ]),
+    new Recipe('tomato pasta', 'tomato Pasta dish', 'http://www.wikihow.com/images/3/38/Make-a-Delicious-Dish-of-Spaghetti-and-Tomato-Sauce-Step-18.jpg', [
+        new Ingredient('pasta', 2),
+        new Ingredient('tomato can', 3)
+    ]),
+    new Recipe('pizza', 'pizza description', 'http://netgeek.biz/wp-content/uploads/2015/12/pizza_newyork5.jpg', [
+        new Ingredient('flour', 50),
+        new Ingredient('pizza source', 1)
+    ])
+  ];
+  constructor() { }
+
+  getRecipes(){
+    return this.recipes;
+  }
+}
