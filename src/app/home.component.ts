8d3b07db0a340e2431a83a09f2af7e72abe3095c
YukiUmetsu <yuuki.umetsu@gmail.com>
2016-10-02 19:51:59 +0900

    separated files into different modules

---
+++ src/app/home.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'rb-home',
+  template: `
+    <h1>
+      Hello To The Recipe Book!
+    </h1>
+  `,
+  styles: []
+})
+export class HomeComponent {
+
+  constructor() { }
+
+}
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,6 @@
+import {NgModule} from "@angular/core/src/metadata/ng_module";
+import {CommonModule} from "@angular/common";
+@NgModule({
+    exports: [CommonModule]
+})
+export class SharedModule{}
